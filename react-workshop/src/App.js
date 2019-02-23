baf52cad4182ca15ffb93d0485d0f32927c14324
slawek367 <slawomir.bando@gmail.com>
2019-02-23 13:45:04 +0100

    Add user search and sort

--- react-workshop/src/App.js
+++ react-workshop/src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import UserList from './UserList'
 
 const users = [
   {
     "id": 1,
-    "name": "aa",
+    "name": "zz",
     "surrname": "bb",
     "phone": 666,
     "favourite": false
@@ -21,8 +20,22 @@ const users = [
   {
     "id": 3,
     "name": "slawek",
-    "surrname": "yy",
+    "surrname": "ydsy",
     "phone": 765656,
+    "favourite": true
+  },
+  {
+    "id": 4,
+    "name": "zzz",
+    "surrname": "gfsgs",
+    "phone": 54245,
+    "favourite": false
+  },
+  {
+    "id": 5,
+    "name": "beata",
+    "surrname": "trwg",
+    "phone": 536653,
     "favourite": true
   }
 ]
@@ -30,7 +43,7 @@ const users = [
 class App extends Component {
   render() {
     return (
-      <div className="App" className="container">
+      <div className="App container">
         <div className="row">
           <div className="col" align="center">
             <h2>Users</h2>
@@ -39,7 +52,7 @@ class App extends Component {
           <div className="col" align="center">
             <h2>Favourite users</h2>
             <UserList users={users.filter(user =>
-              user.favourite == true
+              user.favourite === true
             )} />
           </div>
         </div>
--- react-workshop/src/UserList.js
+++ react-workshop/src/UserList.js
@@ -1,21 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import UserInfo from './UserInfo'
 
-function UserList(props){
+function UserList(props) {
+
+    const [users, setUsers] = useState(props.users)
+    const [sortAsc, setSortAsc] = useState(false)
+    const [value, setValue] = useState("")
+
+    const filterInputUsers = (event) => {
+        let inputName = event.target.value.toLowerCase()
+        if (inputName) {
+            setUsers(props.users.filter(user => {
+                let isOk = false
+                Object.values(user).forEach(val => {
+                    if (val.toString().includes(inputName)) {
+                        isOk = true
+                    }
+                })
+                return isOk
+            }
+            ))
+        } else {
+            setUsers(props.users)
+        }
+        //sortUsers()
+    }
+
+    const sortUsers = () => {
+        let arrCopy = [...users]
+        setUsers(arrCopy.sort((a, b) => sortAsc ? a.name.localeCompare(b.name) : b.name.localeCompare(a.name)))
+    }
+
+    const toggleSort = () => {
+        setSortAsc(!sortAsc)
+        sortUsers()
+    }
 
     return (
-        <table className="table col-sm">
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Surrname</th>
-                    <th>Phone</th>
-                </tr>
-            </thead>
-            <tbody>
-            { props.users.map( user => <UserInfo key={ user.id } user={user}/>) }
-            </tbody>
-        </table>
+        <div>
+            <div className="row">
+                <input className="form-control col-sm" onChange={filterInputUsers} placeholder="Search user"></input>
+            </div>
+            <button onClick={toggleSort}>sort</button>
+            <table className="table col-sm">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Surrname</th>
+                        <th>Phone</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {users.map(user => <UserInfo key={user.id} user={user} />)}
+                </tbody>
+            </table>
+        </div>
     )
 }
 
